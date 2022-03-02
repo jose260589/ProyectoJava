d5cd16902becc083dab7a1a650b2de63521bc355
jose260589 <jose260589@yahoo.com>
2022-03-02 00:18:36 -0400

    PRACTICA DE NOMBRE

---
+++ src/Ejercicios_Practicas/Imprimir_nombre.java
@@ -0,0 +1,10 @@
+package Ejercicios_Practicas;
+
+public class Imprimir_nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jose");
+	}
+
+}
