4bd6a12c569b3ef3aeff6887126387fd5d3b85c5
jose260589 <jose260589@yahoo.com>
2022-03-03 22:43:18 -0400

    se modifico clase furgoneta. agregando un getter.

--- src/poo_practicas/Furgoneta.java
+++ src/poo_practicas/Furgoneta.java
@@ -9,6 +9,16 @@ public class Furgoneta extends Coche {
 	//contructor
 	public Furgoneta(int capacidad_carga, int plazas_extra) {
 		super(); //llamar al constructor de la clase padre padre
+		
+		this.capacidad_carga=capacidad_carga;
+		
+		this.plazas_extra=plazas_extra;
+	}
+	
+	//getter
+	public String dimeDatosFurgoenta() {
+		
+		return "La capacidad de carga es: "+capacidad_carga+ " Y las plazas son: "+plazas_extra;
 	}
 	
 }
