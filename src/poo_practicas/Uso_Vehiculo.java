300ed1c6c6314099c0dc39c7b3313f652c0c4ffa
jose260589 <jose260589@yahoo.com>
2022-03-03 23:37:24 -0400

    se modifico datos de uso vehiculos, ya que habian datos erroneos.

--- src/poo_practicas/Uso_Vehiculo.java
+++ src/poo_practicas/Uso_Vehiculo.java
@@ -4,9 +4,10 @@ public class Uso_Vehiculo {
 
 	public static void main(String[] args) {
 		
-		Coche michoche1= new Coche();
+		Coche micoche1= new Coche();
 		
-		michoche1.estable_color("Rojo");
+		micoche1.estable_color("Rojo");
+		micoche1.configura_climatizador("no");
 		
 		Furgoneta mifurgoneta1=new Furgoneta(580,7);
 		
@@ -14,7 +15,7 @@ public class Uso_Vehiculo {
 		 mifurgoneta1.configura_asientos("si");
 		 mifurgoneta1.configura_climatizador("si");
 		 
-		 System.out.println(michoche1.dime_datos_generales()+"\n"+michoche1.info_color());
+		 System.out.println(micoche1.dime_datos_generales()+"\n"+micoche1.info_color());
 		 
 		 System.out.println(mifurgoneta1.dime_datos_generales()+"\n"+mifurgoneta1.dimeDatosFurgoneta()+
 				 "\n"+mifurgoneta1.info_color());
