09b528d5cbc28f3fb5f175cb3b11de3ef4968ddc
jose260589 <jose260589@yahoo.com>
2022-03-03 22:36:43 -0400

    creando clase furgoneta, y poniendo que herede de clase coche

---
+++ src/poo_practicas/Furgoneta.java
@@ -0,0 +1,14 @@
+package poo_practicas;
+//extends se usa para usar herencia, no se puede hacer herencia multiple
+public class Furgoneta extends Coche {
+
+	private int capacidad_carga;
+	
+	private int plazas_extra;
+	
+	//contructor
+	public Furgoneta(int capacidad_carga, int plazas_extra) {
+		super(); //llamar al constructor de la clase padre padre
+	}
+	
+}
