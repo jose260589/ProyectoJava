509f9b00ad9131eb7fcea8c0e5bdf31a2d14d68f
jose260589 <jose260589@yahoo.com>
2022-03-12 19:25:13 -0400

    Ejercicio de par e impar, pidiendo el numero desde el teclado.

---
+++ src/Ejercicios_Practicas/Par_e_Impar.java
@@ -0,0 +1,22 @@
+package Ejercicios_Practicas;
+
+import java.util.Scanner;
+
+//Este programa es para identificar si un numero por teclado es par o impar
+public class Par_e_Impar {
+		
+	public static void main(String[] args) {
+		
+		Scanner digito=new Scanner(System.in);
+		
+		System.out.println("Digite el numero a validar: ");
+		int num=digito.nextInt();
+		
+		if(num%2==0) {
+			System.out.println("El numero "+num+" es Par");
+		}else {
+			System.out.println("El numero "+num+" es Impar");
+		}
+	}
+
+}
