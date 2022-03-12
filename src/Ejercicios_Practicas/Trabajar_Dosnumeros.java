513257ff94f4a93f4bcc0892e2f6c94cf0292680
jose260589 <jose260589@yahoo.com>
2022-03-12 16:55:39 -0400

    En este archivo, se creo un progrma para trabajar dos numeros, calculos basicos, suma, resta, multiplicacion, division y modulo.

---
+++ src/Ejercicios_Practicas/Trabajar_Dosnumeros.java
@@ -0,0 +1,37 @@
+/*Sumar, restar, multiplicar, dividir y el resto de dos numeros.
+ * */
+
+package Ejercicios_Practicas;
+
+public class Trabajar_Dosnumeros {
+
+	public static void main(String[] args) {
+		
+		double a=5;
+		double b=8;
+		double c=0;
+		
+		//Suma
+		c=a+b;
+		System.out.println("La suma de "+a+"+"+b+" es: "+c);
+		
+		//resta
+		c=b-a;
+		System.out.println("La resta de "+b+"-"+a+" es: "+c);
+		
+		//multiplicacion
+		c=a*b;
+		System.out.println("La multiplicacion de "+a+"*"+b+" es: "+c);
+		
+		//Division
+		c=a/b;
+		System.out.println("La division de "+a+"/"+b+" es: "+c);
+		
+		//pow
+		c=(a%b);
+		System.out.println("residuo es: "+c);
+		
+		
+	}
+
+}
