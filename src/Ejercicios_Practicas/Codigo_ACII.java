32316dad32a40a5ad2985aaa070ff3e08e59d5c0
jose260589 <jose260589@yahoo.com>
2022-03-12 18:40:43 -0400

    Se crea un programa para que muestre el codigo ACII, tambien se hace uso del casting ex.(char)a

---
+++ src/Ejercicios_Practicas/Codigo_ACII.java
@@ -0,0 +1,20 @@
+package Ejercicios_Practicas;
+
+import java.util.Scanner;
+
+public class Codigo_ACII {
+
+	public static void main(String[] args) {
+		
+		Scanner entra=new Scanner(System.in);
+		
+		System.out.println("Introduce un numero para ver el codigo ACII.");
+		int a=entra.nextInt();
+		//asi de facil se cambia a codigo ACII, Esto es un casting
+		char codigo=(char)a;
+		
+		System.out.println("El codigo ACII de: "+a+" es: "+codigo);
+
+	}
+
+}
