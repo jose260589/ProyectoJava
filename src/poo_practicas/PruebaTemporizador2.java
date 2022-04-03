e7d7f997ffc3008d6bc3fac9395a5d958cbd6f15
jose260589 <jose260589@yahoo.com>
2022-04-03 10:21:40 -0400

    se creo una segunda clase, para hacer otro tipo de sonidos, y otras maneras de usarlos.

---
+++ src/poo_practicas/PruebaTemporizador2.java
@@ -0,0 +1,20 @@
+package poo_practicas;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+import java.util.*;
+
+import javax.swing.Timer;
+
+import java.awt.Toolkit;
+
+public class PruebaTemporizador2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
