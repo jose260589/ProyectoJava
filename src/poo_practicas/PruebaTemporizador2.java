02aa63e92154f2c90368c6edc86e0454c3ada371
jose260589 <jose260589@yahoo.com>
2022-04-03 11:29:21 -0400

    se crearon otra clase adicional con un metodo interno, para poder ejecutarse.

--- src/poo_practicas/PruebaTemporizador2.java
+++ src/poo_practicas/PruebaTemporizador2.java
@@ -14,7 +14,52 @@ public class PruebaTemporizador2 {
 
 	public static void main(String[] args) {
 		
+		Reloj mireloj=new Reloj(3000, true);
+		
+		mireloj.enMarcha();
+		
+		JOptionPane.showMessageDialog(null, "Pulsa para terminar");
 
 	}
 
+}
+
+
+class Reloj{
+	
+	private int intervalo;
+	
+	private boolean sonido;
+	
+	public Reloj(int intervalo, boolean sonido) {
+		
+		this.intervalo=intervalo;
+		
+		this.sonido=sonido;
+		
+	}
+	
+	
+	public void enMarcha() {
+		
+		ActionListener oyente=new DameLaHora2();
+		
+		Timer mitemporizador=new Timer(intervalo, oyente);
+		
+		mitemporizador.start();
+	}
+	
+	private class DameLaHora2 implements ActionListener{
+		//metodo constructor
+		public void actionPerformed(ActionEvent evento) {
+			
+			Date ahora=new Date();
+			
+			System.out.println("Te pongo la hora cada 3 segundo: "+ahora);
+			
+			if(sonido==true) {
+				Toolkit.getDefaultToolkit().beep();
+			}
+		}
+	}
 }
